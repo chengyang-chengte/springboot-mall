f873e684eae154026b1c003b2bf3f93a8af80967
chengtechen <61896143+chengtechen@users.noreply.github.com>
2022-03-21 11:10:01 +0800

    第 11 章，第 7單元 - 商品功能（五）-修改商品

--- src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
+++ src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
@@ -18,4 +18,6 @@ public interface ProductDao {
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
--- src/main/java/com/kujudy/springbootmall/service/ProductService.java
+++ src/main/java/com/kujudy/springbootmall/service/ProductService.java
@@ -18,4 +18,6 @@ public interface ProductService {
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
